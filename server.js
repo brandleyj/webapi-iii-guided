b12e7c21f7b9dd4a01bd2d12b95d62132d0131b4
Justin Brandley <brandleyj@gmail.com>
2019-10-30 12:03:45 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
-server.use(express.json());
+function dateLogger(req, res, next) {
+	console.log(new Date().toISOString());
 
-server.use('/api/hubs', hubsRouter);
+	next();
+}
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+function URLLogger(req, res, next) {
+	console.log(`${req.method} ${req.originalUrl}`);
 
-  res.send(`
+	next();
+}
+
+// Global Middleware
+server.use(helmet()); // third party
+server.use(express.json()); // built-in
+server.use(dateLogger); // custom middleware
+server.use(URLLogger);
+
+server.use("/api/hubs", hubsRouter);
+
+server.get("/", (req, res) => {
+	const nameInsert = req.name ? ` ${req.name}` : "";
+
+	res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
